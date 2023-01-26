6ca353d52723399c8e908c7561113e030422d2bd
A Viz <avizer@gmail.com>
2023-01-26 17:20:34 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "lammini-38507",
   url: "https://lammini-38507.botics.co",
   api: "https://lammini-38507.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
